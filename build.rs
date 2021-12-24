5f5eef38efc3a5c31709a9c6e8ea86f200bb8508
tyson <tyson.midboe@gmail.com>
2021-12-24 04:13:11 -0600

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,6 +2,10 @@ use std::path::Path;
 use cc;
 
 fn main() {
+       cc::Build::new()
+        .file("lib/libquickjs.a")
+        .compile("libquickjs");
+    
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let out_dir_path = Path::new(&out_dir);
     std::fs::copy("lib/libquickjs.a", out_dir_path.join("libquickjs.a"))
